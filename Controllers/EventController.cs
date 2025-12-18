785150dd79f82b8b01bda5a8dc1e6504cae92d72
EmelyMachete <134882323+EmelyMachete@users.noreply.github.com>
2025-12-18 12:45:45 +0200

    Add project files.

---
+++ Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventManagementSystem.Models;
+using System.Collections.Generic;
+
+namespace EventManagementSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Temporary List of events (no database yet)
+            var events = new List<Event>
+            {
+                new Event { Id = 1, Name = "Tech Conference", Location = "Johannesburg", Capacity = 100 },
+                new Event { Id = 2, Name = "Business Workshop", Location = "Pretoria", Capacity = 50 },
+            
+            
+            };
+
+            return View(events);
+        }
+}   }
+
+    
+
---
+++ Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EventManagementSystem.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+
+    }
+}
