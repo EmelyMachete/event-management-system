33febab38342dc5a9292950558b9be0db10ebc55
EmelyMachete <134882323+EmelyMachete@users.noreply.github.com>
2025-12-19 11:45:13 +0200

    Add Create Event functionality (CRUD - Create)

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -6,20 +6,36 @@ namespace EventManagementSystem.Controllers
 {
     public class EventController : Controller
     {
+        // Shared in-memory list (temporary, no database yet)
+        private static List<Event> events = new List<Event>
+        {
+            new Event { Id = 1, Name = "Tech Conference", Location = "Johannesburg", Capacity = 100 },
+            new Event { Id = 2, Name = "Business Workshop", Location = "Pretoria", Capacity = 50 }
+        };
+
+        // READ
         public IActionResult Index()
         {
-            // Temporary List of events (no database yet)
-            var events = new List<Event>
-            {
-                new Event { Id = 1, Name = "Tech Conference", Location = "Johannesburg", Capacity = 100 },
-                new Event { Id = 2, Name = "Business Workshop", Location = "Pretoria", Capacity = 50 },
-            
-            
-            };
-
             return View(events);
         }
-}   }
 
-    
+        // CREATE (GET)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE (POST)
+        [HttpPost]
+        public IActionResult Create(Event ev)
+        {
+            ev.Id = events.Count + 1;
+            events.Add(ev);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+
 
